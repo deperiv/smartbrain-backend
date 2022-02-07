1a7648ea71f910a4cee7a12affc845d9aa4c9aab
deperazar <danielpblog2014@gmail.com>
2022-02-07 07:10:00 -0500

    removing bug

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users)})
+app.get('/', (req, res) => { res.send('It is working')})
 
 app.post('/signin', (req, res) => {signIn.handleSignIn(req, res, db, bcrypt)})
 
