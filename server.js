ed5d728a1ba9c7c275a9a42b3da01d38ae7edbd5
deperazar <danielpblog2014@gmail.com>
2022-02-07 07:47:41 -0500

    updating database

--- server.js
+++ server.js
@@ -9,15 +9,16 @@ const profile = require('./controllers/profile')
 const image = require('./controllers/image'); 
 const { database } = require('pg/lib/defaults');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
     client: 'pg',
     connection: {
       connectionString : process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
+      ssl: true
+      // ssl: {
+      //   rejectUnauthorized: false
+      // }
     }
 });
 
